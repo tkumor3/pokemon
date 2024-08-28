e52e8fb43df311b7dde47fac715a658f46a31764
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-08-28 10:34:18 +0200

    redesign home screen

---
+++ src/components/PokemonType.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const PokemonType = ({ type }: { type: string }) => {
+  return (
+    <View style={[styles.container]}>
+      <Text style={styles.text}>{type}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 24,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    backgroundColor: "#ffffff4f",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 12,
+  },
+});
+
+export default PokemonType;
