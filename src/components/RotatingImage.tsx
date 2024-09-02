9a8876286c791eb4439abd62292c3cece9b71fe7
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-09-02 13:17:21 +0200

    feedback

--- src/components/RotatingImage.tsx
+++ src/components/RotatingImage.tsx
@@ -11,23 +11,23 @@ const duration = 2000;
 const easing = Easing.bezier(0.25, -0.5, 0.25, 1);
 
 const RotatingImage = ({ imageUri }: { imageUri?: string }) => {
-  const sv = useSharedValue(0);
+  const rotation = useSharedValue(0);
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotate: `${sv.value * 360}deg` }],
+    transform: [{ rotate: `${rotation.value * 360}deg` }],
   }));
 
   const longPress = Gesture.LongPress()
     .minDuration(2000)
     .onStart(() => {
-      sv.value = withTiming(1, { duration, easing });
+      rotation.value = withTiming(1, { duration, easing });
     })
     .onFinalize(() => {
-      sv.value = 0;
+      rotation.value = 0;
     });
 
   return (
     <GestureDetector gesture={longPress}>
-      <Animated.View style={[animatedStyle]}>
+      <Animated.View style={animatedStyle}>
         <Image
           width={200}
           height={200}
