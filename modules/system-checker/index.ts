b2815ee9995713a810d46df859da2e86bb528f91
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-09-04 16:08:16 +0200

    info about phon

--- modules/system-checker/index.ts
+++ modules/system-checker/index.ts
@@ -1,37 +1,10 @@
-import {
-  NativeModulesProxy,
-  EventEmitter,
-  Subscription,
-} from "expo-modules-core";
-
 // Import the native module. On web, it will be resolved to SystemChecker.web.ts
 // and on native platforms to SystemChecker.ts
 import SystemCheckerModule from "./src/SystemCheckerModule";
-import SystemCheckerView from "./src/SystemCheckerView";
-import {
-  ChangeEventPayload,
-  SystemCheckerViewProps,
-} from "./src/SystemChecker.types";
-
-// Get the native constant value.
-export const PI = SystemCheckerModule.PI;
+import { SystemCheckerViewProps } from "./src/SystemChecker.types";
 
 export function getSystem(): string {
   return SystemCheckerModule.getSystem();
 }
 
-export async function setValueAsync(value: string) {
-  return await SystemCheckerModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(
-  SystemCheckerModule ?? NativeModulesProxy.SystemChecker
-);
-
-export function addChangeListener(
-  listener: (event: ChangeEventPayload) => void
-): Subscription {
-  return emitter.addListener<ChangeEventPayload>("onChange", listener);
-}
-
-export { SystemCheckerView, SystemCheckerViewProps, ChangeEventPayload };
+export { SystemCheckerViewProps };
