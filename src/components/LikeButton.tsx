c315b2986f02396f4f1d7ef486c5a4bf925109bb
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-09-06 15:41:46 +0200

    fix

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -9,11 +9,9 @@ const LikeButton = ({ pokemonId }: { pokemonId?: number }) => {
   const isPokemonLiked = isLiked(pokemonId);
   return (
     <Pressable onPress={() => toggleLike(pokemonId)}>
-      {isPokemonLiked ? (
-        <Text style={{ color: colors.colorRevert, fontSize: 24 }}>♥</Text>
-      ) : (
-        <Text style={{ color: colors.colorRevert, fontSize: 24 }}>♡</Text>
-      )}
+      <Text style={{ color: colors.colorRevert, fontSize: 24 }}>
+        {isPokemonLiked ? "♥" : "♡"}
+      </Text>
     </Pressable>
   );
 };
